87662ff3f118c9232e274b9fddf690a7ecc7a436
cdwhitt <cwhitt91@gmail.com>
2019-11-06 11:37:35 -0500

    Quick image fixes

--- app/javascript/react/components/LandingPage.js
+++ app/javascript/react/components/LandingPage.js
@@ -9,12 +9,12 @@ const LandingPage = (props) => {
           <h1 className="welcome-header">TRAVELexp</h1>
           <div className="row">
             <div className="columns large-6">
-              <img src="/assets/undraw_traveling_t8y2 (1).svg"/>
+              <img src="https://travelexpbucket.s3.amazonaws.com/travelexp/undraw_traveling_t8y2+(1).svg"/>
               <h2>Already a Member?</h2>
               <a id="sign-in" href="/users/sign_in?">Sign In</a>
             </div>
             <div className="columns large-6 right-splash">
-              <img src="/assets/undraw_around_the_world_v9nu.svg"/>
+              <img src="https://travelexpbucket.s3.amazonaws.com/travelexp/undraw_around_the_world_v9nu.svg"/>
               <h2>New to TRAVELexp?</h2>
               <a id="sign-up" href="/users/sign_up?">Sign Up</a>
             </div>
